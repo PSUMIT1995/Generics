98ebed975484d9130c1424a50c198e8368643343
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-27 23:07:47 +0530

    Uc1 : Find the maximum integer number

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaximumNumber.FindMax(19, 11, 28)) ;
+        }
+    }
+}
